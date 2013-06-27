cc5e1eae032885006bdcc330b25693e7f84606a3
Will Luo <will.luo@gigaom.com>
2013-06-26 17:20:38 -0700

    differentiate the join clause generated by co-author-plus from the ones generated by WP. this allows us to query for author and some other taxonomy terms when the co-author-plus plugin is enabled.

--- co-authors-plus.php
+++ co-authors-plus.php
@@ -544,8 +544,8 @@ class coauthors_plus {
 				return $join;
 
 			// Check to see that JOIN hasn't already been added. Props michaelingp and nbaxley
-			$term_relationship_join = " INNER JOIN {$wpdb->term_relationships} ON ({$wpdb->posts}.ID = {$wpdb->term_relationships}.object_id)";
-			$term_taxonomy_join = " INNER JOIN {$wpdb->term_taxonomy} ON ( {$wpdb->term_relationships}.term_taxonomy_id = {$wpdb->term_taxonomy}.term_taxonomy_id )";
+			$term_relationship_join = " INNER JOIN {$wpdb->term_relationships} AS catr ON ({$wpdb->posts}.ID = catr.object_id)";
+			$term_taxonomy_join = " INNER JOIN {$wpdb->term_taxonomy} ON ( catr.term_taxonomy_id = {$wpdb->term_taxonomy}.term_taxonomy_id )";
 
 			if( strpos( $join, trim( $term_relationship_join ) ) === false ) {
 				$join .= str_replace( "INNER JOIN", "LEFT JOIN", $term_relationship_join );
